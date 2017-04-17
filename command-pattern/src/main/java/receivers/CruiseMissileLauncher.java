a92d0a3841154bad9f70781a302e7efe4ca35ce3
Nirjal Khadka <nirjal@gmail.com>
2017-04-17 23:49:42 +0200

    no message

--- command-pattern/src/main/java/receivers/CruiseMissileLauncher.java
+++ command-pattern/src/main/java/receivers/CruiseMissileLauncher.java
@@ -4,7 +4,7 @@ package receivers;
  * Created by nirjal on 4/17/2017.
  */
 public class CruiseMissileLauncher {
-    
+
 
     public void launchCruiser() {
         System.out.println("Cruise missile launched");
