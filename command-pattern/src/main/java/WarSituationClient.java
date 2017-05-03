7b28274584693a4f2c1eead262fe8321b4297799
nirjal <nirjal.khadka@uc.se>
2017-05-03 14:43:44 +0200

    Added comments

--- command-pattern/src/main/java/WarSituationClient.java
+++ command-pattern/src/main/java/WarSituationClient.java
@@ -12,20 +12,20 @@ public class WarSituationClient {
 
    public static void main( String [] args)
    {
-
+       //Receivers
        CruiseMissileLauncher cruiseMissileLauncher = new CruiseMissileLauncher();
        NuclearMissileLauncher nuclearMissileLauncher = new NuclearMissileLauncher();
 
+       //Concrete commands
        Command launchCruise = new CruiserCommand(cruiseMissileLauncher);
        Command launchNuclear = new NuclearCommand(nuclearMissileLauncher);
 
+       //Invoker
        President goodPresident = new President(launchCruise);
        President badPresident = new President(launchNuclear);
 
        goodPresident.execute();
        badPresident.execute();
-
-
    }
 
 }
